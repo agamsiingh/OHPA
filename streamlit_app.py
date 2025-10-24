8eeb56d7a318bc8ff02e2b6b36d32e5b9c3dbef0
Agam singh <116442467+agamsiingh@users.noreply.github.com>
2025-10-24 09:53:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import plotly.graph_objects as go
 from datetime import datetime, timedelta
 import os
 
-API_BASE_URL = os.getenv('API_BASE_URL', 'http://localhost:8000')
+
 
 st.set_page_config(
     page_title="Oilseed Hedging Platform",
